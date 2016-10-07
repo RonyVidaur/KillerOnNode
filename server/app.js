952a85858d082308ba7a8fa7308a6ebd7d05c398
RonyVidaur <rony.vidaur@gmail.com>
2016-10-06 21:24:25 -0600

    html page to be served

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const http = require('http')
+const fs = require('fs')
+
 let server = http.createServer((req, res)=>{
   console.log(`request was made: ${req.url}`)
-  res.writeHead(200, {'Content-Type': "text/plain"})
-  res.end('See how cool this is')
+  res.writeHead(200, {'Content-Type': "application/json"})
+  let mObject = {
+    name: 'Rony',
+    job: 'Ninja',
+    age: 23
+  };
+  //let myReadStream = fs.createReadStream(__dirname + '/index.html','utf8')
+  //myReadStream.on('data',(chunk)=>{mWriteStream.write(chunk)})
+  //myReadStream.pipe(res);
+  res.end(JSON.stringify(mObject));
 })
 
 server.listen(3000, '127.0.0.1')
