e315010391359b4e8a0f4c4a2c0bbcbe38a082dc
RonyVidaur <rony.vidaur@gmail.com>
2016-10-22 21:18:51 -0600

    serving html instead of txt

--- server/app.js
+++ server/app.js
@@ -8,8 +8,8 @@ myReadStream.pipe(myWriteStream)
 */
 let server = http.createServer((req,res)=>{
   console.log(`request was made: ${req.url}`)
-  res.writeHead(200, {'Content-Type' : 'text/plain'})
-  let myReadStream = fs.createReadStream(`${__dirname}/readMe.txt`,'utf8')
+  res.writeHead(200, {'Content-Type' : 'text/html'})
+  let myReadStream = fs.createReadStream(`${__dirname}/index.html`,'utf8')
   myReadStream.pipe(res)
 })//this basically serves the text file to the client
 server.listen(3000, '127.0.0.1');
